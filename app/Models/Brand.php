06ce8889aa3c31db4dd73bd02e5d2fdf5ed32c6c
Dzikri Khasnudin <dzikri.khasnudin2@gmail.com>
2025-04-03 22:05:21 +0700

    membuat table migration dan mass assignment pada model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Brand extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    // mass assignment
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo'
+    ];
+
+    public function shoes(): HasMany
+    {
+        return $this->hasMany(Shoe::class);
+    }
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
 }
--- database/migrations/2025_04_03_140726_create_shoes_table.php
+++ database/migrations/2025_04_03_140726_create_shoes_table.php
@@ -13,6 +13,18 @@ return new class extends Migration
     {
         Schema::create('shoes', function (Blueprint $table) {
             $table->id();
+
+            $table->string('name');
+            $table->string('slug');
+            $table->string('thumbnail');
+            $table->text('about');
+            $table->unsignedBigInteger('price');
+            $table->unsignedBigInteger('stock');
+            $table->boolean('is_popular');
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained()->cascadeOnDelete();
+            $table->softDeletes();
+
             $table->timestamps();
         });
     }
