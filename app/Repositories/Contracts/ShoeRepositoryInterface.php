8a5708d66a873666c69e478c2861b7c9ea136e53
Dzikri Khasnudin <dzikri.khasnudin2@gmail.com>
2025-04-16 21:21:16 +0700

    membuat Service dan Repositories serta Front Services

---
+++ app/Repositories/Contracts/ShoeRepositoryInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+interface ShoeRepositoryInterface
+{
+    public function getPopularShoes($limit);
+
+    public function getAllNewShoes();
+
+    public function find($id);
+
+    public function getPrice($ticketId);
+
+    public function searchByName(string $keyword);
+}
